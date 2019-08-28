b3333e9329ab90d9127678d9a307891c3f0ab48d
masan67 <mariosanchezramirez67@gmail.com>
2019-08-28 01:06:08 -0500

    ya jalo

--- app/src/main/java/com/marioapps/webviewbridge/MainActivity.java
+++ app/src/main/java/com/marioapps/webviewbridge/MainActivity.java
@@ -3,18 +3,22 @@ package com.marioapps.webviewbridge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.webkit.ValueCallback;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.EditText;
-import com.marioapps.webviewbridge.WebAppInterface;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private WebView webView;
-    private EditText edit_text_to_web;
+    private Button btnWV, btnJSON;
+    public static TextView textView;
+    private ScrollView scrollView;
+
     private String url = "https://notingerapp.000webhostapp.com/javascript_test.html";
 
     @Override
@@ -23,9 +27,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.webview);
-        edit_text_to_web = findViewById(R.id.edit_text_to_web);
+        btnWV = findViewById(R.id.buttonwebview);
+        btnJSON = findViewById(R.id.buttonJSON);
+        textView = findViewById(R.id.textview);
+        scrollView = findViewById(R.id.scrollview);
 
-        //webView.setWebViewClient(new WebViewClient());
+        btnWV.setOnClickListener(this);
+        btnJSON.setOnClickListener(this);
+
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
@@ -33,5 +42,25 @@ public class MainActivity extends AppCompatActivity {
         webView.addJavascriptInterface(new WebAppInterface(getApplicationContext()),"Android");
         webView.loadUrl(url);
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonwebview:
+                webView.setVisibility(View.VISIBLE);
+                scrollView.setVisibility(View.GONE);
+                break;
+
+            case R.id.buttonJSON:
+                webView.setVisibility(View.GONE);
+                scrollView.setVisibility(View.VISIBLE);
+                fetchData process = new fetchData();
+                process.execute();
+                break;
+
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/marioapps/webviewbridge/fetchData.java
@@ -0,0 +1,76 @@
+package com.marioapps.webviewbridge;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class fetchData extends AsyncTask<Void,Void,Void> {
+    String data;
+    String dataParsed = "";
+    String singleParsed = "";
+    String[] arrayStrings;
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+        try {
+            URL url = new URL("https://api.myjson.com/bins/qwpvj");
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while (line != null) {
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+
+            String str = "geekss@for@geekss";
+            arrayStrings = data.split("null");
+
+
+
+
+            JSONArray ja = new JSONArray(arrayStrings[1]);
+            for(int i=0;i<ja.length();i++){
+                JSONObject jo = (JSONObject) ja.get(i);
+                singleParsed = "Name:"+jo.get("name")+"\n"+
+                        "Password:"+jo.get("password")+"\n"+
+                        "Contact:"+jo.get("contact")+"\n"+
+                        "Country:"+jo.get("country")+"\n";
+
+
+                dataParsed = dataParsed + singleParsed +"\n";
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        MainActivity.textView.setText(dataParsed);
+    }
+
+}
+
